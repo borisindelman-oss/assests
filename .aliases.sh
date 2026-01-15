12f340debf4987b0b1bb56c9e148d7634033011a
Boris Indelman <boris.indelman@wayve.ai>
2026-01-15 20:36:03 +0000

    git sync 2026-01-15T20:36:03Z

--- .aliases.sh
+++ .aliases.sh
@@ -55,7 +55,7 @@ train_parking() {
         return 1
     fi
     session_tag="$1"
-    cmd="bazel run //wayve/ai/si/cli:cli -- --no-verify --experiment parking --platform AKS --cluster dgx-h100 --num_nodes 4 --project parking --priority P1 +mode=parking_bc_train model.model.gear_direction_dropout_probability=0.1 --force --session_tag \"$session_tag\""
+    cmd="bazel run //wayve/ai/si/cli:cli -- --no-verify --experiment parking --platform AKS --cluster dgx-h100 --num_nodes 4 --project parking --priority P1 +mode=parking_bc_train_wfm_october_2025 model.model.gear_direction_dropout_probability=0.1 --force --session_tag \"$session_tag\""
     echo "$cmd"
     eval $cmd
 }
