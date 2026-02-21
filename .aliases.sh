7023784ca32233184f68d5b0c877121e37b6f489
Boris Indelman <boris.indelman@wayve.ai>
2026-02-21 19:03:04 +0000

    git sync 2026-02-21T19:03:04Z

--- .aliases.sh
+++ .aliases.sh
@@ -74,9 +74,7 @@ start_jupyter() {
 }
 
 alias claude-npm='npx @anthropic-ai/claude-code@latest'
-alias codex-wayve='cd /workspace/WayveCode/ && bazel run //tools:codex -- --add-dir /home/borisindelman/git/assets/codex --cd /workspace/WayveCode/ --add-dir /home/borisindelman/git/vault --add-dir /workspace/WayveCode/ --add-dir /workspace/WayveCode/ /home/borisindelman/.codex --ask-for-approval on-failure --search '
-alias codex-last='codex-wayve resume --last'
-alias codexx='cd /workspace/WayveCode/ && /bin/codex --add-dir /home/borisindelman/git/assets/codex --cd /workspace/WayveCode/ --add-dir /home/borisindelman/git/vault --add-dir /workspace/WayveCode/ --add-dir /workspace/WayveCode/ /home/borisindelman/.codex --yolo --search '
+alias codex='cd /workspace/WayveCode/ && /bin/codex --add-dir /home/borisindelman/git/assets/codex --cd /workspace/WayveCode/ --add-dir /home/borisindelman/git/vault --add-dir /workspace/WayveCode/ --add-dir /workspace/WayveCode/ /home/borisindelman/.codex --yolo --search '
 export CLAUDE_CONFIG_DIR=/workspace/WayveCode/.claude
 
 # Wayve terminal logo (uv)
